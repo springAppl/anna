04f28c8ec800d5d0a293a47eff97e12d19b4c029
liushaofei <liushaofei@qujiali.com>
2018-09-06 16:22:59 +0800

    use kuboot

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const ApiRootUrl = 'http://119.29.105.18/api/';
+const ApiRootUrl = 'https://kuboot.cn/api/';
 //const ApiRootUrl = 'http://127.0.0.1:8080/api/';
 module.exports = {
   baseURL: ApiRootUrl
